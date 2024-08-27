d9b3e754244a113c64d40a124f8deef715aa0069
Parvin Malekzadeh <45303081+pmalekzadeh@users.noreply.github.com>
2024-08-27 12:28:43 -0400

    Delete scripts directory

--- scripts/evt_robustness/evt_bsm_robustness.sh
+++
@@ -1,9 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-for vol in {1..8}; do
-    python run_d3pg.py @run_configs/agents/d3pg.cfg --env_config=run_configs/envs/bsm_vanilla_eval.yaml --logger_prefix=evt/bsm_vanilla/bsm_vol0.$vol/rl --bsm_portfolio.client_trade_poisson_rate=1.0 --bsm_sde.vol=0.$vol --eval_env.vega_ratio=False --train_env.vega_ratio=False
-done
-
-for vol in {1..8}; do
-    python run_benchmark.py @run_configs/agents/gamma.cfg --env_config=run_configs/envs/bsm_vanilla_eval.yaml --logger_prefix=evt/bsm_vanilla/bsm_vol0.$vol/gamma --bsm_portfolio.client_trade_poisson_rate=1.0 --bsm_sde.vol=0.$vol --eval_env.vega_ratio=False --train_env.vega_ratio=False
-done
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-python run_d3pg.py @run_configs/agents/d3pg.cfg --env_config=run_configs/test_envs/train_bsm_0.3_test_bsm.yaml --logger_prefix=logs/bsm/train_bsm_0.3_test_bsm
-python run_benchmark.py @run_configs/agents/gamma.cfg --env_config=run_configs/test_envs/train_bsm_0.3_test_bsm.yaml --logger_prefix=logs/bsm/gamma
-
-python run_d3pg.py @run_configs/agents/d3pg.cfg --env_config=run_configs/test_envs/train_bsm_0.3_test_sabr.yaml --logger_prefix=logs/sabr/train_bsm_0.3_test_realdata --eval_only --agent_path=logs/bsm/train_bsm_0.3_test_bsm
-python run_benchmark.py @run_configs/agents/gamma.cfg --env_config=run_configs/test_envs/train_bsm_0.3_test_sabr.yaml --logger_prefix=logs/sabr/gamma
-
-python run_d3pg.py @run_configs/agents/d3pg.cfg --env_config=run_configs/test_envs/train_bsm_0.3_test_realdata.yaml --logger_prefix=logs/realdata/train_bsm_0.3_test_realdata --eval_only --agent_path=logs/bsm/train_bsm_0.3_test_bsm
-python run_benchmark.py @run_configs/agents/gamma.cfg --env_config=run_configs/test_envs/train_bsm_0.3_test_realdata.yaml --logger_prefix=logs/realdata/gamma
